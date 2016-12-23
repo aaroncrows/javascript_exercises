d7482d827dd5de7dcfce49b19546de0e626899a3
Johnny Luangphasy <johnny@jluangphasy.com>
2016-12-22 22:14:29 -0800

    Complete exercise 2

--- ex_2.js
+++ ex_2.js
@@ -6,7 +6,10 @@
 //eg: [1, 2, 3] -> 6
 //[3, 4, 1] -> 8
 function addNumbers(array) {
-  //your code here!
+  return array
+    .reduce(function (a, b) {
+      return a + b;
+    });
 };
 
 //Write a function that tells you whether an array contains a given
@@ -15,14 +18,17 @@ function addNumbers(array) {
 //    ['slugs are better whatever'], 'sloth' -> true
 
 function hasItem(array, match) {
-  //your code here
+  return array
+    .indexOf(match);
 };
 
 //Write a function that tells you the highest value in a given array
 //eg [2, 5, 9] -> 9
 // [6, 4, 6] -> 6
 function highestNumber(array) {
-  //your code here
+  return array
+    .sort()
+    .slice(-1);
 };
 
 //Write a function that adds an s to the end of each string in an array
@@ -30,7 +36,11 @@ function highestNumber(array) {
 //eg: ['sloth', 'slug', 'bat'] -> ['sloths', 'slugs', 'bats']
 //eg: ['snakes', 'baby'] -> ['snakess', 'babys']
 function pluralize(array) {
-  //your code here
+  return array
+    .map(function (string) {
+      return string
+        .concat('s');
+    });
 };
 
 exports.addNumbers = addNumbers;
